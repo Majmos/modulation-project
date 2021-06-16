402b22d2630173aa82fafe64ad4dfbc29afb1626
Majmos <mimsowy@gmail.com>
2021-06-16 14:07:10 +0200

    Added SER

--- src/askmod.py
+++ src/askmod.py
@@ -3,6 +3,9 @@ import random
 import sys
 from ast import literal_eval as make_tuple
 import matplotlib
+import numpy as np #for numerical computing
+import matplotlib.pyplot as plt #for plotting functions
+from scipy.special import erfc #erfc/Q function
 
 import komm
 
@@ -82,26 +85,87 @@ def modulations_testing():
                 else:
                     continue
                 err_sum = 0.0
+                sym_err_sum = 0.0
                 channel = komm.AWGNChannel(arguments[2])
                 for j in range(100):
                     modulated = modulation.modulate(inputs)
                     transmitted = channel(modulated)
                     demodulated = modulation.demodulate(transmitted)
                     errors = 0
+                    symbol_errors = 0
+                    index = 0
+                    skip_next = False
                     for d, i in zip(demodulated, inputs):
                         if d != i:
                             errors += 1
+                        if d != i and index % 2 == 0:
+                            symbol_errors += 1
+                            skip_next = True
+                        elif d != i and skip_next == False:
+                            symbol_errors += 1
+                        skip_next = False
+                        index += 1
                     error_rate = errors / input_len * 100
                     err_sum += error_rate
+                    symbol_error_rate = symbol_errors / (input_len / 2) * 100
+                    sym_err_sum += symbol_error_rate
                 error_rate = err_sum / 100
-                # print(f"Orders: {orders}")
-                # print(f"Amplitudes: {amplitudes}")
-                # print(f"Input size: {input_len}")
-                # print(f"Signal to noise ratio: {arguments[2]}")
-                # print(f"Error rate: {round_half_up(error_rate, 2)}%")
+                symbol_error_rate = sym_err_sum / 100
+                print(f"Orders: {orders}")
+                print(f"Amplitudes: {amplitudes}")
+                print(f"Input size: {input_len}")
+                print(f"Signal to noise ratio: {arguments[2]}")
+                print(f"Bit error rate: {round_half_up(error_rate, 2)}%")
+                print(f"Symbol error rate: {round_half_up(symbol_error_rate, 2)}%")
                 r.write(f"{orders};{amplitudes};{input_len};{arguments[2]};{round_half_up(error_rate, 2)}\n")
     f.close()
     r.close()
 
 
+def TEST():
+    # ---------Input Fields------------------------
+    nSym = 10 ** 3  # Number of symbols to transmit
+    EbN0dBs = np.arange(start=-4, stop=13, step=2)  # Eb/N0 range in dB for simulation
+    BER_sim = np.zeros(len(EbN0dBs))  # simulated Bit error rates
+
+    M = 2  # Number of points in BPSK constellation
+    m = np.arange(0, M)  # all possible input symbols
+    A = 1;  # amplitude
+    constellation = A * np.cos(m / M * 2 * np.pi)  # reference constellation for BPSK
+
+    # ------------ Transmitter---------------
+    inputSyms = np.random.randint(low=0, high=M, size=nSym)  # Random 1's and 0's as input to BPSK modulator
+    s = constellation[inputSyms]  # modulated symbols
+
+    fig, ax1 = plt.subplots(nrows=1, ncols=1)
+    ax1.plot(np.real(constellation), np.imag(constellation), '*')
+
+    # ----------- Channel --------------
+    # Compute power in modulatedSyms and add AWGN noise for given SNRs
+    for j, EbN0dB in enumerate(EbN0dBs):
+        gamma = 10 ** (EbN0dB / 10)  # SNRs to linear scale
+        P = sum(abs(s) ** 2) / len(s)  # Actual power in the vector
+        N0 = P / gamma  # Find the noise spectral density
+        n = np.sqrt(N0 / 2) * np.random.standard_normal(s.shape)  # computed noise vector
+        r = s + n  # received signal
+
+    # -------------- Receiver ------------
+    detectedSyms = (r <= 0).astype(int)  # thresolding at value 0
+    BER_sim[j] = np.sum(detectedSyms != inputSyms) / nSym  # calculate BER
+
+    BER_theory = 0.5 * erfc(np.sqrt(10 ** (EbN0dBs / 10)))
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.semilogy(EbN0dBs, BER_sim, color='r', marker='o', linestyle='', label='BPSK Sim')
+    ax.semilogy(EbN0dBs, BER_theory, marker='', linestyle='-', label='BPSK Theory')
+    ax.set_xlabel('$E_b/N_0(dB)$')
+    ax.set_ylabel('BER ($P_b$)')
+    ax.set_title('Probability of Bit Error for BPSK over AWGN channel')
+    ax.set_xlim(-5, 13)
+    ax.grid(True)
+    ax.legend()
+    plt.show()
+
+
 modulations_testing()
+# TEST()
